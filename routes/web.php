770b2cc230d8415b5ba4e51b8e9044b10ecc0bac
A. William <ashibuogwuwilliams64@gmail.com>
2026-08-30 21:41:12 +0100

    security: route contract downloads through authorization

--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,18 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])
-        ->name('dashboard');
-
+    Route::get('/contracts/{contract}/document', [ContractController::class, 'download'])
+        ->name('contracts.document.download');
     Route::resource('contracts', ContractController::class);
 
     Route::resource('categories', CategoryController::class)
-        ->only([
-            'index',
-            'create',
-            'store',
-            'destroy',
-        ]);
+        ->only(['index', 'create', 'store', 'destroy']);
 
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
